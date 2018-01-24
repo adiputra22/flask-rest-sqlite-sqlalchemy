3fcc87c50192113bac83195b09582c8e7e0f7bf1
Adiputra <adiputra@bambideal.com>
2018-01-24 19:39:04 +0700

    adding run.py at uwsgi heroku

--- app.py
+++ app.py
@@ -14,10 +14,6 @@ app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.secret_key = "adiputra"
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
